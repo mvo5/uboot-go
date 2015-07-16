4d26686bf36d6aacfc3f0fbb7d45bc3c64e87131
Michael Vogt <mvo@ubuntu.com>
2015-07-16 18:11:23 +0200

    make set work

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
+	"strings"
 )
 
 // FIXME: add config option for that so that the user can select if
@@ -64,11 +65,79 @@ func printEnv(env *EnvHeader) error {
 	return nil
 }
 
+func setEnv(env *EnvHeader, name, value string) error {
+	envStrs := []string{}
+
+	for _, envStr := range bytes.Split(env.data, []byte{0}) {
+		if len(envStr) == 0 || envStr[0] == 0 || envStr[0] == 255 {
+			continue
+		}
+		// remove str we want to rewrite
+		if strings.HasPrefix(string(envStr), fmt.Sprintf("%s=", value)) {
+			continue
+		}
+		envStrs = append(envStrs, string(envStr))
+	}
+
+	// append new str
+	if value != "" {
+		envStrs = append(envStrs, fmt.Sprintf("%s=%s", name, value))
+	}
+
+	w := bytes.NewBuffer(nil)
+	for _, envStr := range envStrs {
+		//println(envStr)
+		fmt.Fprintf(w, "%s", envStr)
+		w.Write([]byte{0})
+	}
+	pad := make([]byte, len(env.data)-w.Len())
+	w.Write(pad)
+	env.data = w.Bytes()
+	env.crc = crc32.ChecksumIEEE(env.data)
+
+	return nil
+}
+
+func writeUint32(u uint32) []byte {
+	buf := bytes.NewBuffer(nil)
+	binary.Write(buf, binary.LittleEndian, &u)
+	return buf.Bytes()
+}
+
+func writeUbootEnv(envFile string, env *EnvHeader) error {
+	f, err := os.Create(envFile)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	f.Write(writeUint32(env.crc))
+	f.Write([]byte{env.flags})
+	f.Write(env.data)
+
+	return nil
+}
+
 func main() {
+	// FIXME: argsparse ftw!
 	envFile := os.Args[1]
 	env, err := readUbootEnv(envFile)
 	if err != nil {
-		log.Fatalf("readUboot failed for %s: %s", envFile, err)
+		log.Fatalf("readUbootEnv failed for %s: %s", envFile, err)
 	}
-	printEnv(env)
+
+	switch os.Args[2] {
+	case "print":
+		printEnv(env)
+	case "set":
+		name := os.Args[3]
+		value := os.Args[4]
+		if err := setEnv(env, name, value); err != nil {
+			log.Fatalf("setEnv failed: %s", err)
+		}
+		if err := writeUbootEnv(envFile, env); err != nil {
+			log.Fatalf("writeUbootEnv failed for %s: %s", envFile, err)
+		}
+	}
+
 }
