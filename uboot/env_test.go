f22ec6af3b3d7b407f68ba9f3ada63dee01d62f2
Michael Vogt <mvo@ubuntu.com>
2015-07-17 11:11:48 +0200

    another test

--- uboot/env_test.go
+++ uboot/env_test.go
@@ -27,3 +27,15 @@ func (u *ubootTestSuite) TestSetNoDuplicate(c *C) {
 	env.Set("foo", "bar")
 	c.Assert(env.String(), Equals, "foo=bar\n")
 }
+
+func (u *ubootTestSuite) TestOpenEnv(c *C) {
+	env, err := CreateEnv(u.envFile, 4096)
+	c.Assert(err, IsNil)
+	env.Set("foo", "bar")
+	c.Assert(env.String(), Equals, "foo=bar\n")
+	env.Write()
+
+	env2, err := OpenEnv(u.envFile)
+	c.Assert(err, IsNil)
+	c.Assert(env2.String(), Equals, "foo=bar\n")
+}
